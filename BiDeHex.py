293652724a71a958085bd5fb036f794cf6c4b956
TheLuctus <65297796+TheLuctus@users.noreply.github.com>
2020-05-31 20:31:48 +0200

    Update BiDeHex.py

--- BiDeHex.py
+++ BiDeHex.py
@@ -1,38 +1,35 @@
-question_one = ""
-question = ""
+choice_of_numeral_system = ""
 
 
-def question_two(x):
-    q = input("Write the letter r to Reset the program or e to exit\n")
-    if q == "r":
-        print("\n\n\n\n\n\n\n\n\n\n\n\n")
-    if q == "e":
+def exit_or_reset(x):
+    if x == "e":
         exit()
 
 
-while question_one != "b" or question_one != "d" or question_one != "h":
+while choice_of_numeral_system != "b" or choice_of_numeral_system != "d" or choice_of_numeral_system != "h":
     while True:
         try:
-            question_one = input("What numeral system is ur number in?\nBinary = b\nDecimal = d\nHexadecimal = h\n")
+            choice_of_numeral_system = input("What numeral system is your number in?\n"
+                                             "Binary = b\nDecimal = d\nHexadecimal = h\n")
             break
         except ValueError:
-            print("death")
+            print("ValueError\n Restart Program")
 
-    if question_one == "b":
-        ur_number = input("What is ur number?\n")
-        decimal_representation = int(ur_number, 2)
-        print("The Decimal value of the number is", decimal_representation)
-        print("The Hexadecimal value of the number is", hex(decimal_representation))
-        question_two(question)
+    if choice_of_numeral_system == "b":
+        ur_number = input("What's your number?\n")
+        decimal_number = int(ur_number, 2)
+        print("The Decimal value of the number is", decimal_number)
+        print("The Hexadecimal value of the number is", hex(decimal_number))
+        exit_or_reset(input("Enter the letter e to exit the program.\nEnter anything to reset the program.\n"))
 
-    elif question_one == "d":
-        ur_number = int(input("What is ur number?\n"))
+    elif choice_of_numeral_system == "d":
+        ur_number = int(input("What's your number?\n"))
         print("The Binary value of the number is", bin(ur_number))
         print("The Hexadecimal value of the number is", hex(ur_number))
-        question_two(question)
+        exit_or_reset(input("Enter the letter e to exit the program.\nEnter anything to reset the program.\n"))
 
-    elif question_one == "h":
-        ur_number = input("What is ur number?\n")
+    elif choice_of_numeral_system == "h":
+        ur_number = input("What's your number?\n")
         print("The Decimal value of the number is", int(ur_number, 16))
         print("The Binary value of the number is", bin(int(ur_number, 16)))
-        question_two(question)
+        exit_or_reset(input("Enter the letter e to exit the program.\nEnter anything to reset the program.\n"))
