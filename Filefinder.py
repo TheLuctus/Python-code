3642b4e07bf9bc36f3dd517a497089df07dc97e1
TheLuctus <65297796+TheLuctus@users.noreply.github.com>
2020-08-27 20:40:39 +0200

    Update Filefinder.py

--- Filefinder.py
+++ Filefinder.py
@@ -1,23 +1,23 @@
-import os
-import string
-import pprint
+import os, string as st, pprint as pp
 
 searched_file = ""
 path_container = []
 choice_list = []
 choice_list_dict = {}
 users_file_choice = ""
-available_drives = ['%s:\\' % d for d in string.ascii_uppercase if os.path.exists('%s:\\' % d)]     #variables
+available_drives = ['%s:\\' % d for d in st.ascii_uppercase if os.path.exists('%s:\\' % d)]
+step = "1"
 
-
-def file_search_process(x):                                                                         #file_search_process function
-    for r, d, f in os.walk(x):                                                                      #.walk methode
+def file_search_process(x):
+    for r, d, f in os.walk(x):
         if searched_file in f:
             path_container.append(os.path.join(r, searched_file))
-            for x in path_container:
-                choice_list.append(x)
+            for st in path_container:
+                new_string = st.replace("\\", "/")
+                choice_list.append(new_string)
+
             for i in range(len(choice_list)):
-                choice_list_dict[i] = choice_list[i]
+                choice_list_dict[i] = choice_list[i] 
 
 
 def open_read_file(x):
@@ -31,28 +31,26 @@ def root_paths(i):
     for i in available_drives:
         file_search_process(i)
 
-
-while True:
+while step == "1":
     try:
-        searched_file = input("Put in the full name of the file your searching for: ")
-        print("Rooting...")
+        searched_file = input("\n\n\nTyp the file name with file extention, that you're searching for: ")
+        print("\nRooting...\n")
         root_paths(available_drives)
-        while choice_list_dict:
-            if True:
-                pprint.pprint(choice_list_dict, indent=4)
-                print("\nRooting Finished.")
-                try:
-                    users_file_choice = int(input("Choose a number from above: "))
-                    if users_file_choice not in choice_list_dict.keys():
-                        print("Please use a numeral value in range.")
-                    if users_file_choice in choice_list_dict.keys():
-                        if True:
-                            print("Pls wait for the reading process...\n")
-                            open_read_file(choice_list_dict[users_file_choice])
-                        exit(input("\nFinished reading.\nPress enter to exit program. "))
-                except ValueError:
-                    print("Please use a numeral value.")
+        if choice_list_dict:
+            step = "2"
         if not choice_list_dict:
-            print("No such file found.\nTry again.")
-    except TypeError:
-        print("TypeError: Restart program")
+            print("\n!!!No such file found!!!\n\nPlease try again.")
+
+        while step == "2":
+            try:
+                pp.pprint(choice_list_dict, indent=4)
+                users_file_choice = int(input("\nChoose a number from above: "))
+                if users_file_choice in choice_list_dict.keys():
+                    print("\nPls wait for the reading process...\n")
+                    open_read_file(choice_list_dict[users_file_choice])
+                    print("\n\nReading process: Done!\n\n")
+                    step = input("Type 1 to keep searching,\nType 2 to select another file to read,\nPress enter to exit: ")
+            except ValueError:
+                print("\n"* 10 + "!!!Please use a numeral value in range!!!\n")
+    except KeyboardInterrupt:
+        exit()   
