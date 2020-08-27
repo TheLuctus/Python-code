b55926ed9feeb6431fbb4c349d0dd9035d925c91
TheLuctus <65297796+TheLuctus@users.noreply.github.com>
2020-08-27 20:37:01 +0200

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,4 +1,4 @@
-from random import *
+import random
 
 container = ""
 counter = 0
@@ -8,29 +8,22 @@ symbol_list = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
                "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "{",
                "}", "(", ")", "[", "]", "#", ":", ";", "^", ",", ".", "?", "!", "|", "&", "_", "'", "~", "@", "$", "%",
                "/", "=", "+", "-", "`")
-while True:
+               
+while password == "":
     try:
         len_of_password = int(input("How many characters should your new password have?\n"
                                     "Choose a number between 8 to 72?: "))
-        break
+
+        if len_of_password < 8 or len_of_password > 72:
+            print("I said from 8-72.")
+
+        if 8 <= len_of_password <= 72:
+            while len_of_password > counter:
+                counter = counter + 1
+                symbol_picker = random.randint(0, 89)
+                container = symbol_list[symbol_picker]
+                password = password + container
+            print(f"This is your new password: {password}")
+            exit(input("Don`t forget it(write it down)!\nPress Enter to exit"))
     except ValueError:
         print("Only numbers!\nTry again.")
-
-while len_of_password < 8 or len_of_password > 72:
-    print("I said from 8-72.")
-    while True:
-        try:
-            len_of_password = int(input("How many characters should your new password have?\n"
-                                        "Choose a number between 8 to 72?: "))
-            break
-        except ValueError:
-            print("Only numbers!\nTry again.")
-
-if 8 <= len_of_password <= 72:
-    while len_of_password > counter:
-        counter = counter + 1
-        symbol_picker = randint(0, 89)
-        container = symbol_list[symbol_picker]
-        password = password + container
-    print(f"This is your new password: {password}")
-    exit(input("Don`t forget it(write it down)!\nPress Enter to exit"))
